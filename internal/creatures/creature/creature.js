77043e33f57063a0e550232a30edd672b06e0d72
El-Chapo0133 <levequeloris66@gmail.com>
2021-03-17 13:45:20 +0100

    Added some ideas and the mocha lib

--- internal/creatures/creature/creature.js
+++ internal/creatures/creature/creature.js
@@ -1,15 +1,19 @@
 
 
 class Creature {
-        constructor() {
+        constructor(parent1, parent2) {
                 this.name;
                 this.gender;
-                this.experience;
+                this.experience = 0;
                 this.parents;
                 this.stats;
+                this.bornTime;
+
+                this.init();
+        }
+        init(parent1, parent2) {
                 
         }
-
 
 }
 
---
+++ test/main.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const Creature = require('./../internal/creatures/creature/creature.js');
+
+
+// CREATURE SECTION
+describe('Creature', () => {
+    describe("Creation", () => {
+        let creature1 = new Creature();
+        let creature2 = new Creature();
+    });
+});
